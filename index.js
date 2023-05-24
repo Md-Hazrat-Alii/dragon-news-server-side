1013dcb6f99fb33401fa5398b8b18b8d032b2f39
Md Hazrat Ali <hazratali4936@gmail.com>
2023-05-24 08:32:17 +0200

    basic backend server for dragon news update

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 
-const categories = require('./data/categories.json');
-const news = require('./data/news.json');
+const categories = require("./data/categories.json");
+const news = require("./data/news.json");
 
-app.get('/', (req, res) => {
-    res.send('News API Running');
+app.get("/", (req, res) => {
+  res.send("News API Running"); //we can also use res.josn
 });
 
-app.get('/news-categories', (req, res) => {
-    res.send(categories)
+app.get("/news-categories", (req, res) => {
+  res.send(categories);
 });
 
 app.listen(port, () => {
-    console.log('Dragon News Server running on port', port);
-})
+  console.log("Dragon News Server running on port", port);
+});
